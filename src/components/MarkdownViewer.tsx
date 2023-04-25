1cb45ced3cb2b1b3c9f0db87a003b086d072686c
asd34256 <ara34256@snu.ac.kr>
2023-04-25 10:56:46 +0900

    feat: add dopamine post

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -3,12 +3,11 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import Image from 'next/image';
 
 export default function MarkdownViewer({ content }: { content: string }) {
   return (
     <ReactMarkdown
-      className='prose max-w-none'
+      className='prose max-w-none m-8'
       remarkPlugins={[remarkGfm]}
       components={{
         code({ node, inline, className, children, ...props }) {
@@ -28,15 +27,6 @@ export default function MarkdownViewer({ content }: { content: string }) {
             </code>
           );
         },
-        img: (image) => (
-          <Image
-            className='w-full max-h-60 object-cover'
-            src={image.src || ''}
-            alt={image.alt || ''}
-            width={500}
-            height={350}
-          />
-        ),
       }}
     >
       {content}
--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -6,13 +6,13 @@ export default function PostContent({ post }: { post: PostData }) {
   const { title, description, date, content } = post;
   return (
     <section className='flex flex-col p-4 w-full'>
-      <div className='flex items-center self-end text-sky-600'>
+      <div className='flex items-center self-end text-sky-600 mx-8'>
         <AiTwotoneCalendar />
         <p className='font-semibold ml-2'>{date.toString()}</p>
       </div>
-      <h1 className='text-4xl font-bold'>{title}</h1>
-      <p className='text-xl font-bold'>{description}</p>
-      <div className='w-44 border-2 border-sky-600 mt-4 mb-8' />
+      <h1 className='text-4xl font-bold pb-4 px-8'>{title}</h1>
+      <p className='text-xl font-bold px-8'>{description}</p>
+      <div className='w-6/12 border-2 border-sky-600 mt-4 mb-8' />
       <MarkdownViewer content={content} />
     </section>
   );
